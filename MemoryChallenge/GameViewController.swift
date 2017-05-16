90d97a83a5d5c6ff29971bcdd091a6a360b3637f
王冠綸 <ToriWang@wangguanlunde-MacBook-Pro.local>
2017-05-16 15:27:58 +0800

    完成數字鍵盤

--- MemoryChallenge/GameViewController.swift
+++ MemoryChallenge/GameViewController.swift
@@ -11,13 +11,17 @@ import UIKit
 class GameViewController: UIViewController {
     
     @IBOutlet weak var backButtonOutlet: UIButton!
-    
     @IBOutlet weak var useTimeLabel: UILabel!
     @IBOutlet weak var minuteLabel: UILabel!
     @IBOutlet weak var colonLabel: UILabel!
     @IBOutlet weak var secondLabel: UILabel!
     @IBOutlet weak var correctCountStackView: UIStackView!
-    
+    @IBOutlet weak var mainButtonOutlet: UIButton!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var mainQuestionLabel: UILabel!
+    @IBOutlet weak var subQuestionLabel: UILabel!
+    @IBOutlet weak var questionMarkLabel: UILabel!
+    @IBOutlet weak var numberPadStackView: UIStackView!
     
     var level: Int!
     var timeCounting = 1
@@ -34,6 +38,10 @@ class GameViewController: UIViewController {
         startCountdown()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
     func isHidden(status: Bool) {
         backButtonOutlet.isHidden = status
         useTimeLabel.isHidden = status
@@ -41,6 +49,12 @@ class GameViewController: UIViewController {
         colonLabel.isHidden = status
         secondLabel.isHidden = status
         correctCountStackView.isHidden = status
+        mainButtonOutlet.isHidden = status
+        answerLabel.isHidden = status
+        mainQuestionLabel.isHidden = status
+        subQuestionLabel.isHidden = status
+        questionMarkLabel.isHidden = status
+        numberPadStackView.isHidden = status
     }
 
     func startCountdown() {
@@ -88,6 +102,31 @@ class GameViewController: UIViewController {
             self.timeCounting += 1
         }
     }
+    
+    @IBAction func mainButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func numberPad(_ sender: UIButton) {
+        switch sender.tag {
+        case 1...9:
+            if (answerLabel.text?.characters.count)! < 2 {
+                answerLabel.text?.append(String(sender.tag))
+            }
+        case 10:
+            if (answerLabel.text?.characters.count)! < 2 {
+                answerLabel.text?.append("0")
+            }
+        case 11:
+            if answerLabel.text != "" {
+                let ansString = answerLabel.text!
+                answerLabel.text = ansString[ansString.startIndex..<ansString.index(before: ansString.endIndex)]
+            }
+        default:
+            break
+        }
+    }
+    
     
     @IBAction func backButton(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
